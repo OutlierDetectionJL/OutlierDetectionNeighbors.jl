ec03204a1fec808af7dd41a711794b31a56d2a3d
David Muhr <muhrdavid+github@gmail.com>
2021-09-16 14:27:38 +0200

    fix model exports

--- src/OutlierDetectionNeighbors.jl
+++ src/OutlierDetectionNeighbors.jl
@@ -16,19 +16,20 @@ module OutlierDetectionNeighbors
     include("models/knn.jl")
     include("models/lof.jl")
 
-    MODELS = (ABODDetector,
-              COFDetector,
-              DNNDetector,
-              KNNDetector,
-              LOFDetector)
+    const UUID = "51249a0a-cb36-4849-8e04-30c7f8d311bb"
+    const MODELS = [:ABODDetector,
+                    :COFDetector,
+                    :DNNDetector,
+                    :KNNDetector,
+                    :LOFDetector]
 
-    org = "OutlierDetectionJL"
-    uuid = "51249a0a-cb36-4849-8e04-30c7f8d311bb"
     for model in MODELS
-        @eval(export $model)
-        OD.metadata_pkg(model, package_name=@__MODULE__, package_uuid=uuid,
-                        package_url="https://github.com/$org/$(@__MODULE__).jl",
-                        is_pure_julia=true, package_license="MIT", is_wrapper=false)
-        OD.load_path(::Type{model}) = "$(@__MODULE__).$model"
+        @eval begin
+            OD.metadata_pkg($model, package_name=string(@__MODULE__), package_uuid=$UUID,
+                            package_url="https://github.com/OutlierDetectionJL/$(@__MODULE__).jl",
+                            is_pure_julia=true, package_license="MIT", is_wrapper=false)
+            OD.load_path(::Type{$model}) = string($model)
+            export $model
+        end
     end
 end
